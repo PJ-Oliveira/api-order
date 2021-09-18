a2235edcd16823e4ec2f9b1ca8bb43636aab63ca
Guilherme Assis <guilherme.assis@compasso.com.br>
2021-09-18 16:23:41 -0300

    Resolvendo confflito de versões

---
+++ order/src/main/java/com/shadow/order/advice/OrderControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.shadow.order.advice;
+
+import com.shadow.order.advice.exception.OrderException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class OrderControllerAdvice
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+            (value = { IllegalArgumentException.class, IllegalStateException.class, OrderException.class, Exception.class})
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException runtimeException, WebRequest request) {
+        return handleExceptionInternal(runtimeException, null,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+    @ExceptionHandler(javax.validation.ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException constraintViolationException){
+        List<String> errors = new ArrayList<>(constraintViolationException.getConstraintViolations().size());
+        constraintViolationException.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_GATEWAY);
+    }
+}
--- order/src/main/java/com/shadow/order/configuration/OfferClientConfig.java
+++ order/src/main/java/com/shadow/order/configuration/OfferClientConfig.java
@@ -5,17 +5,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 
 @FeignClient(value = "${feign.offer.name}", url = "${feign.offer.url}")
 public interface OfferClientConfig {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}", consumes ="application/json")
-    Offer findOneOffer(@PathVariable("id")@Valid Long id);
+    Offer findOneOffer(@PathVariable("id")@NotNull  @Valid Long id);
 
-    /*@RequestMapping(method = RequestMethod.GET, value = "exist/{id}", consumes ="application/json")
-    Long exist(@PathVariable("id")@Valid Long id);*/
 
 }
